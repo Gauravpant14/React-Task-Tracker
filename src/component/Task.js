20ffe1ae8c72d0abc4859eb33e168f319864e28b
Gaurav <aadeezcity@gmail.com>
2021-03-10 13:16:37 +0530

    Here we have used state

--- src/component/Task.js
+++ src/component/Task.js
@@ -1,27 +1,27 @@
-const tasks = [
-    {
-        id:1,
-        text:'DOC APPOt',
-        day:'feb 5th',
-        reminder : true,
-    },
-    {
-        id:2,
-        text:'App APPOt',
-        day:'feb 6th',
-        reminder : true,
-    },
-    {
-        id:3,
-        text:'Meeting APPOt',
-        day:'feb 7th',
-        reminder : true,
-    }
-]
+import { useState } from 'react'
 
-//Here we have used Key keyword and have assigned id to that key, because each list should have a unique key otherwise it'll give error.
+const Tasks = () => {
+    const [ tasks, setTasks] = useState([
+        {
+            id:1,
+            text:'DOC APPOt',
+            day:'feb 5th',
+            reminder : true,
+        },
+        {
+            id:2,
+            text:'App APPOt',
+            day:'feb 6th',
+            reminder : true,
+        },
+        {
+            id:3,
+            text:'Meeting APPOt',
+            day:'feb 7th',
+            reminder : true,
+        }
+    ])
 
-const Task = () => {
     return (
         <>
           {tasks.map((task) => (
@@ -32,4 +32,4 @@ const Task = () => {
     )
 }
 
-export default Task
+export default Tasks
